681fa3aeb6c683b72710172eeaa85add9b958126
zotics <18087814+hoysics@users.noreply.github.com>
2022-12-22 15:25:20 +0800

    feat: try trait

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -185,25 +185,48 @@
 //     fib_for(n);
 // }
 
-fn main() {
-    let data = vec![1, 2, 3, 4];
-    let data1 = &data;
-    // 值的地址是什么？引用的地址又是什么？
-    println!(
-        "addr of value: {:p}({:p}), addr of data {:p}, data1: {:p}",
-        &data, data1, &&data, &data1
-    );
-    println!("sum of data1: {}", sum(data1));
+// fn main() {
+//     let data = vec![1, 2, 3, 4];
+//     let data1 = &data;
+//     // 值的地址是什么？引用的地址又是什么？
+//     println!(
+//         "addr of value: {:p}({:p}), addr of data {:p}, data1: {:p}",
+//         &data, data1, &&data, &data1
+//     );
+//     println!("sum of data1: {}", sum(data1));
 
-    // 堆上数据的地址是什么？
-    println!(
-        "addr of items: [{:p}, {:p}, {:p}, {:p}]",
-        &data[0], &data[1], &data[2], &data[3]
-    );
+//     // 堆上数据的地址是什么？
+//     println!(
+//         "addr of items: [{:p}, {:p}, {:p}, {:p}]",
+//         &data[0], &data[1], &data[2], &data[3]
+//     );
+// }
+
+// fn sum(data: &Vec<u32>) -> u32 {
+//     // 值的地址会改变么？引用的地址会改变么？
+//     println!("addr of value: {:p}, addr of ref: {:p}", data, &data);
+//     data.iter().fold(0, |acc, x| acc + x)
+// }
+
+pub fn strtok<'a>(s: &mut &'a str, delimiter: char) -> &'a str {
+    if let Some(i) = s.find(delimiter) {
+        let prefix = &s[..i];
+        // 由于 delimiter 可以是 utf8，所以我们需要获得其 utf8 长度，
+        // 直接使用 len 返回的是字节长度，会有问题
+        let suffix = &s[(i + delimiter.len_utf8())..];
+        *s = suffix;
+        prefix
+    } else {
+        // 如果没找到，返回整个字符串，把原字符串指针 s 指向空串
+        let prefix = *s;
+        *s = "";
+        prefix
+    }
 }
 
-fn sum(data: &Vec<u32>) -> u32 {
-    // 值的地址会改变么？引用的地址会改变么？
-    println!("addr of value: {:p}, addr of ref: {:p}", data, &data);
-    data.iter().fold(0, |acc, x| acc + x)
+fn main() {
+    let s = "hello world".to_owned();
+    let mut s1 = s.as_str();
+    let hello = strtok(&mut s1, ' ');
+    println!("hello is: {}, s1: {}, s: {}", hello, s1, s);
 }
